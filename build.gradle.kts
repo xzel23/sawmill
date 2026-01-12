5bbfa2df02d3ac624a59fc3f57efc95758701932
Axel Howind <axh@dua3.com>
2026-01-12 01:59:55 +0100

    Configure Gradle with `com.dua3.gradle.jdkprovider` plugin and set JDK 21 with bundled JavaFX

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
+plugins {
+    id("com.dua3.gradle.jdkprovider") version "0.4.0"
+}
+
 allprojects {
+    apply(plugin = "com.dua3.gradle.jdkprovider")
+
+    jdk {
+        version = 21
+        javaFxBundled = true
+    }
+
     repositories {
         mavenCentral()
     }
